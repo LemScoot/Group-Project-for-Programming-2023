36348e87de95b3e907ad01b2db883a14da429af9
Lem_Scoot <89105667+LemScoot@users.noreply.github.com>
2023-04-13 10:53:23 -0600

    Add files via upload

---
+++ src/Snake.py
@@ -0,0 +1,35 @@
+# Liam Scott | 3/16/23 | Computer Programming
+
+class Snake:
+    
+    x = 0
+    y = 0
+    w = 5
+    h = 5
+  
+def Snake(self):
+    self.snake.shape('square')
+    self.snake.color("white")
+    self.snake.penup()
+    self.snake.goto(self.x,self.y)
+  
+    def draw(self):
+        self.snake.goto(self.x,self.y)
+
+    def move(self):
+        if(keyboard.is_pressed('w')):
+            self.y = self.y - 0.1
+        if(keyboard.is_pressed('s')):
+            self.y = self.y + 0.1
+        if(keyboard.is_pressed('a')):
+            self.x = self.x - 0.1
+        if(keyboard.is_pressed('d')):
+            self.x = self.x + 0.1
+            self.snake.goto(self.x,self.y)
+            
+    def display(self):
+        rect()
+        noStroke()
+        fill(255)
+        rect(x, y, w, h)
+        
