f58d448c634be3b68b446f474e3c9ba6cc37aed2
Lem_Scoot <89105667+LemScoot@users.noreply.github.com>
2023-04-20 10:26:13 -0600

    Add files via upload

---
+++ src/Food.py
@@ -0,0 +1,24 @@
+class Food(object):
+    x = 0
+    y = 0
+    diam = 0
+    
+    def Food(self):
+        self.x = 500
+        self.y = -100
+        self.diam = int(10,10)
+        self.rand = int(random(3))
+        if (rand == 0):
+            food = loadImage("food.png")
+            
+    def display(self):
+        imageMode(CENTER)
+        food.resize(diam, diam)
+        image(food,x,y)
+        
+    def move(self):
+        self.x
+        self.y
+        for i in range(x, y):
+            r = random(x, y)
+            r.display()
